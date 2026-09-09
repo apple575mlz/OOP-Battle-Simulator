673e42cf227f6fbd059a10136e88479f1d106609
apple575mlz <austin.appletoft@gmail.com>
2026-09-09 10:37:07 -0500

    game.py Practice Changes
    Customized the arena name, introduction, and the goblin name

--- game.py
+++ game.py
@@ -1,16 +1,16 @@
 from goblin import Goblin
 
 
-ARENA_NAME = "The Iron Circle"
+ARENA_NAME = "The Gates of Hades"
 
 
 def main():
     """Open the arena and introduce its first opponent."""
-    print(f"Welcome to {ARENA_NAME}!")
+    print(f"Be prepared for {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
-    goblin = Goblin("Gribble")
+    goblin = Goblin("Goblinstein")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print("But no hero has answered the call... yet.")
